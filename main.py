150d8f79547894a842c13f0fe9d79e4c9d3f7193
nayfaan <luknnn129@gmail.com>
2022-01-24 09:34:14 -0500

    interface ready

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import tkinter as tk
 
+def output_area():
+    frame_l = tk.Frame(master = root)
+    frame_r = tk.Frame(master = root, borderwidth = 2, width = 5*box_size)
+    frame_r.pack_propagate(0)
+    frame_l.pack(fill=tk.Y, side=tk.LEFT)
+    frame_r.pack(fill=tk.Y, side=tk.LEFT)
+    
+    return (frame_l, frame_r)
+
 def to_uppercase(var, index):
     low_alp = "abcdefghijklmnopqrstuvwxyz"
     up_alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -25,29 +34,48 @@ def force_upper(entry_var):
         for ind, alpha in enumerate(row):
             alpha.trace("w", lambda name, index, mode, ind=ind, alpha=alpha: to_uppercase(alpha, ind))
             
-def new_row():
+            
+def cycle_color(i):
+    color_list = [white, yellow, green]
+    
+    box_color = entry_box[-1][i]["background"]
+    entry_box[-1][i]["background"] = color_list[(color_list.index(box_color) + 1) % len(color_list)]
+
+def disable_last_row():
     try:
-        for box in entry_box[-1]:
-            box_color = box['background']
-            box['state'] = 'disabled'
-            if box_color != "white":
-                box["disabledbackground"] = box_color
+        for i in range(5):
+            box_color = entry_box[-1][i]["background"]
+            
+            cycle_btn[-1][i].master.destroy()
+            
+            entry_box[-1][i]["state"] = "disabled"
+            if box_color != white:
+                entry_box[-1][i]["disabledbackground"] = box_color
             else:
-                box["disabledbackground"] = "grey"
+                entry_box[-1][i]["disabledbackground"] = "grey"
     except:
         pass
+
+def new_row(submit_btn, frame_l):
+    disable_last_row()
     
     entry_var.append([])
     entry_box.append([])
+    cycle_btn.append([])
     
-    frame_1 = tk.Frame(master = root, borderwidth = 1, padx=box_size/10, pady=box_size/10)
+    frame_1 = tk.Frame(master = frame_l, borderwidth = 1, padx=box_size/10, pady=box_size/10)
     for i in range(5):
         #entry box
         entry_frame = tk.Frame(master = frame_1, relief=tk.RAISED, borderwidth = 1, width=box_size, height=box_size)
         entry_frame.pack_propagate(0)
         
         entry_var[-1].append(tk.StringVar(root))
-        entry = tk.Entry(master = entry_frame, width = 1, font=("Imperial", int(0.7*box_size)), justify='center', textvariable = entry_var[0][i], background = "white")
+        entry = tk.Entry(master = entry_frame,
+                         width = 1,
+                         font=("Imperial", int(0.7*box_size)),
+                         justify='center',
+                         textvariable = entry_var[len(entry_var)-1][i],
+                         background = white)
         
         entry_box[-1].append(entry)
         
@@ -60,21 +88,42 @@ def new_row():
         cycle_frame.pack_propagate(0)
         
         cycle = tk.Button(master = cycle_frame, width = 1, height = 1)
+        cycle_btn[-1].append(cycle)
+        cycle.bind("<Button-1>", lambda event, i=i: cycle_color(i))
+        
         cycle.pack(fill="both", expand=True)
         cycle_frame.grid(row = 1, column = i)
-        
     frame_1.pack()
+    
+    submit_btn.pack_forget()
+    submit_btn.pack()
+    
+def submit_btn_press(submit_btn, frame_l):
+    if len(entry_box) < 6:
+        new_row(submit_btn, frame_l)
+    else:
+        disable_last_row()
+        submit_btn.destroy()
 
 def init():
     global entry_var
     entry_var = []
     global entry_box
     entry_box = []
-    new_row()
-
+    global cycle_btn
+    cycle_btn = []
     
+    main_frame = (frame_l, frame_r) = output_area()
     
-    #submit_btn = tk.Button(text="send", highlightbackground='#3E4149').pack()
+    submit_btn = tk.Button(master = frame_l, text="send", highlightbackground='#3E4149')
+    submit_btn.bind("<Button-1>", lambda event, submit_btn=submit_btn, frame_l=frame_l: submit_btn_press(submit_btn, frame_l))
+    
+    output_word_list = "Possible words:\n\n" + ", ".join(sol)
+    output_word_list_label = tk.Text(master = frame_r, width = 5*box_size)#, state = "disabled")
+    output_word_list_label.insert("1.0", output_word_list)
+    output_word_list_label.pack()
+    
+    new_row(submit_btn, frame_l)
 
 def run():
     global root
@@ -91,5 +140,11 @@ if __name__ == "__main__":
         
     global box_size
     box_size = 60
+    global white
+    global yellow
+    global green
+    white = "white"
+    yellow = "#b59f3b"
+    green = "#538d4e"
     
     run()
